824244afeefb7699b5c40fdbc5f633dd4d394048
inzahgi <inzahgi@126.com>
2023-02-08 22:54:44 +0800

    saveqq add pic

---
+++ chapter05/saveqq/app/src/main/java/com/example/saveqq/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.saveqq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
