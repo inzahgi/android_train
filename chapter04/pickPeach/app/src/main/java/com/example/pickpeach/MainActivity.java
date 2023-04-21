1db512629db28ed61b2e9bf177e6a5e04adb2480
inzahgi <inzahgi@126.com>
2023-04-21 16:04:18 +0800

    add directory

--- chapter04/pickPeach/app/src/main/java/com/example/pickpeach/MainActivity.java
+++ chapter04/pickPeach/app/src/main/java/com/example/pickpeach/MainActivity.java
@@ -15,11 +15,15 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_peach;
     private TextView tv_count;
     private int totalCount = 0;
+
+    private int[] peachResArray = new int[10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,41 +35,44 @@ public class MainActivity extends AppCompatActivity {
     private void init() {
         btn_peach = findViewById(R.id.btn_peach);
         tv_count = findViewById(R.id.tv_count);
+        //todo new method
+        ActivityResultLauncher<Intent> intentActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if(result.getData() != null && result.getResultCode() == 1) {
+
+                            int count = result.getData().getIntExtra("count", 0);
+                            peachResArray =  result.getData().getIntArrayExtra("peachResArray");
+
+                            totalCount = totalCount + count;
+                            tv_count.setText("摘到" + totalCount + "个");
+                        }
+                    }
+                }
+        );
         btn_peach.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, PeachActivity.class);
-                startActivityForResult(intent, 1);
+                intent.putExtra("peachResArray", peachResArray);
+                //startActivityForResult(intent, 1);
 
-                //todo new method
-//                ActivityResultLauncher<Intent> intentActivityResultLauncher = registerForActivityResult(
-//                        new ActivityResultContracts.StartActivityForResult(),
-//                        new ActivityResultCallback<ActivityResult>() {
-//                            @Override
-//                            public void onActivityResult(ActivityResult result) {
-//                                if(result.getData() != null && result.getResultCode() == Activity.RESULT_OK) {
-//                                    //String data = result.getData().getStringExtra("count");
-//                                    //Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
-//                                    int count = result.getData().getIntExtra("count", 0);
-//                                    totalCount = totalCount + count;
-//                                    tv_count.setText("摘到" + totalCount + "个");
-//                                }
-//                            }
-//                        }
-//                );
-//                intentActivityResultLauncher.launch(intent);
+
+                intentActivityResultLauncher.launch(intent);
             }
         });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == 1 && resultCode == 1) {
-            int count = data.getIntExtra("count", 0);
-            totalCount = totalCount + count;
-            tv_count.setText("摘到" + totalCount + "个");
-        }
-    }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if(requestCode == 1 && resultCode == 1) {
+//            int count = data.getIntExtra("count", 0);
+//            totalCount = totalCount + count;
+//            tv_count.setText("摘到" + totalCount + "个");
+//        }
+//    }
 }
--- chapter04/pickPeach/app/src/main/java/com/example/pickpeach/PeachActivity.java
+++ chapter04/pickPeach/app/src/main/java/com/example/pickpeach/PeachActivity.java
@@ -2,6 +2,7 @@ package com.example.pickpeach;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
@@ -10,15 +11,23 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Arrays;
+
 public class PeachActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button btn_one, btn_two, btn_three, btn_four, btn_five, btn_six, btn_exit;
+    private Button btn_one, btn_two, btn_three, btn_four, btn_five, btn_six, btn_exit, btn_restart;
     private int count = 0;
+
+    private int[] peachResArray = null;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_peach);
+        Intent intent = getIntent();
+        peachResArray =  intent.getIntArrayExtra("peachResArray");
+        Log.i("test", "line = 29 peachResArray:"+ Arrays.toString(peachResArray));
+
         init();
     }
 
@@ -30,6 +39,8 @@ public class PeachActivity extends AppCompatActivity implements View.OnClickList
         btn_five = findViewById(R.id.btn_five);
         btn_six = findViewById(R.id.btn_six);
         btn_exit = findViewById(R.id.btn_exit);
+        btn_restart = findViewById(R.id.btn_restart);
+
         btn_one.setOnClickListener(this);
         btn_two.setOnClickListener(this);
         btn_three.setOnClickListener(this);
@@ -37,6 +48,39 @@ public class PeachActivity extends AppCompatActivity implements View.OnClickList
         btn_five.setOnClickListener(this);
         btn_six.setOnClickListener(this);
         btn_exit.setOnClickListener(this);
+        btn_restart.setOnClickListener(this);
+
+
+        initPeach();
+    }
+
+    private void initPeach() {
+        for (int i = 0; i < 6; i++) {
+            if(peachResArray[i] != 0) {
+                Log.i("test", "line = 50\t" +i);
+                switch (i) {
+                    case 0:
+                        btn_one.setVisibility(View.INVISIBLE);
+                        break;
+                    case 1:
+                        btn_two.setVisibility(View.INVISIBLE);
+                        break;
+                    case 2:
+                        btn_three.setVisibility(View.INVISIBLE);
+                        break;
+                    case 3:
+                        btn_four.setVisibility(View.INVISIBLE);
+                        break;
+                    case 4:
+                        btn_five.setVisibility(View.INVISIBLE);
+                        break;
+                    case 5:
+                        btn_six.setVisibility(View.INVISIBLE);
+                        break;
+                    default:
+                }
+            }
+        }
     }
 
     @Override
@@ -44,25 +88,39 @@ public class PeachActivity extends AppCompatActivity implements View.OnClickList
         switch (view.getId()){
             case R.id.btn_one:    //第一个桃子的点击事件
                 info(btn_one);
+                peachResArray[0] = 1;
                 break;
             case R.id.btn_two:    //第二个桃子的点击事件
                 info(btn_two);
+                peachResArray[1] = 1;
                 break;
             case R.id.btn_three:  //第三个桃子的点击事件
                 info(btn_three);
+                peachResArray[2] = 1;
                 break;
             case R.id.btn_four:   //第四个桃子的点击事件
                 info(btn_four);
+                peachResArray[3] = 1;
                 break;
             case R.id.btn_five:   //第五个桃子的点击事件
                 info(btn_five);
+                peachResArray[4] = 1;
                 break;
             case R.id.btn_six:    //第六个桃子的点击事件
                 info(btn_six);
+                peachResArray[5] = 1;
                 break;
             case R.id.btn_exit:   //“退出桃园”按钮的点击事件
                 returnData();
                 break;
+            case R.id.btn_restart:
+                peachResArray = new int[6];
+                btn_one.setVisibility(View.VISIBLE);
+                btn_two.setVisibility(View.VISIBLE);
+                btn_three.setVisibility(View.VISIBLE);
+                btn_four.setVisibility(View.VISIBLE);
+                btn_five.setVisibility(View.VISIBLE);
+                btn_six.setVisibility(View.VISIBLE);
         }
     }
 
@@ -70,11 +128,13 @@ public class PeachActivity extends AppCompatActivity implements View.OnClickList
         count++;
         btn.setVisibility(View.INVISIBLE);
         Toast.makeText(PeachActivity.this, "摘到" + count + "个桃子",
-                Toast.LENGTH_LONG).show();
+                Toast.LENGTH_SHORT).show();
     }
     private void returnData() {
         Intent intent = new Intent();
         intent.putExtra("count" ,count);
+        intent.putExtra("peachResArray", peachResArray);
+        Log.i("test", "returnData: count:"+ count + "peachResArray:"+ Arrays.toString(peachResArray));
         setResult(1, intent);
         PeachActivity.this.finish();
     }
