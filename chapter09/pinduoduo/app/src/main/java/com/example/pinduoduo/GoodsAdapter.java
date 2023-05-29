781f019d3d6f21d84c9cece8fba947bbf142224e
inzahgi <inzahgi@126.com>
2023-05-29 21:35:26 +0800

    fix chapter09

--- chapter09/pinduoduo/app/src/main/java/com/example/pinduoduo/GoodsAdapter.java
+++ chapter09/pinduoduo/app/src/main/java/com/example/pinduoduo/GoodsAdapter.java
@@ -1,13 +1,15 @@
 package com.example.pinduoduo;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.bumptech.glide.Glide;
 import java.util.ArrayList;
 import java.util.List;
